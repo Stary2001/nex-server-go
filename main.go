f64d9f5380452a31f8cd02173ece04fc332a1c56
Ezekiel Bethel <zek@9net.org>
2018-12-24 15:40:36 +0000

    Secure connection works

--- main.go
+++ main.go
@@ -6,11 +6,95 @@ import (
 	NEX "github.com/Stary2001/nex-go"
 )
 
+func nexServerHandleData(Client *NEX.Client, Packet *NEX.Packet) {
+	//stream := NEX.NewInputStream(Packet.RMCRequest.Parameters)
+	var response NEX.RMCResponse
+	//responseStream := NEX.NewOutputStream()
+
+	ResponsePacket := NEX.NewPacket(Client)
+
+	ResponsePacket.SetVersion(0)
+	ResponsePacket.SetSource(Packet.Destination)
+	ResponsePacket.SetDestination(Packet.Source)
+	ResponsePacket.SetType(NEX.Types["Data"])
+
+	pid := 1234567890          // account PID/NEX username (dummy)
+	password := "nex_password" // account NEX password (dummy)
+
+	key := []byte(password)
+
+	for i := 0; i < 65000+pid%1024; i++ {
+		key = NEX.MD5Hash(key)
+	}
+
+	//Kerberos := NEX.NewKerberos(string(key))
+	proto_id := Packet.RMCRequest.ProtocolID & ^uint8(0x80)
+	proto, ok := NEXProtocols.Protocols[proto_id]
+
+	if ok {
+		method, ok := proto.Methods[Packet.RMCRequest.MethodID]
+		if ok {
+			response = method(Packet.RMCRequest)
+		} else {
+			// response := failure
+			panic(fmt.Sprintf("Unimplemented method %08x in protocol %02x!", Packet.RMCRequest.MethodID, proto_id))
+		}
+	} else {
+		// response := failure
+		panic(fmt.Sprintf("Unimplemented protocol id %02x!", proto_id))
+	}
+
+	ResponsePacket.SetPayload(response.Bytes())
+	Client.Server.Send(Client, &ResponsePacket)
+}
+
+func secureServerHandleConnect(Client *NEX.Client, Packet *NEX.Packet) {
+	stream := NEX.NewInputStream(Packet.Payload)
+	/*ticketData*/ _ = stream.Buffer()
+	checkData := stream.Buffer()
+
+	secureKey := ""
+	// TODO: use the ticket data to lookup a proper session key. We return 16/32 zeroes atm.
+	if Client.Server.Settings.KerberosKeySize == 16 {
+		secureKey = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+	} else {
+		secureKey = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+	}
+
+	Client.SetKey(secureKey)
+
+	Kerberos := NEX.NewKerberos(secureKey)
+	checkDataDecrypted := Kerberos.Decrypt(checkData)
+	checkStream := NEX.NewInputStream(checkDataDecrypted)
+	_ = NEX.PID(checkStream.UInt32LE())
+	_ = checkStream.UInt32LE()
+	check := checkStream.UInt32LE()
+
+	
+	outStream := NEX.NewOutputStream()
+	outStream.UInt32LE(check + 1)
+	outRealStream := NEX.NewOutputStream()
+	outRealStream.Buffer(outStream.Bytes())
+
+	Client.Server.AcknowledgeWithPayload(Packet, outRealStream.Bytes())
+}
+
+func createNexServer(settings NEX.Settings) *NEX.Server {
+	Server := NEX.NewServer(settings)
+	Server.On("Packet", func(Client *NEX.Client, Packet *NEX.Packet) {
+		if Packet.HasFlag(NEX.Flags["NeedAck"]) && Packet.Type != NEX.Types["Connect"]{
+			Server.Acknowledge(Packet)
+		}
+	})
+
+	Server.On("Data", nexServerHandleData);
+	return Server;
+}
+
 func main() {
 	NEXProtocols.RegisterProtocols()
 
 	settings := NEX.NewSettings()
-
 	settings.PrudpVersion = 0
 	settings.PrudpV0SignatureVersion = 1
 	settings.KerberosKeySize = 16
@@ -18,55 +102,14 @@ func main() {
 
 	fmt.Println("starting memes")
 
-	Server := NEX.NewServer(settings)
-
-	Server.On("Packet", func(Client *NEX.Client, Packet *NEX.Packet) {
-		if Packet.HasFlag(NEX.Flags["NeedAck"]) {
-			Server.Acknowledge(Packet)
-		}
+	AuthServer := createNexServer(settings)
+	AuthServer.On("Connect",  func(Client *NEX.Client, Packet *NEX.Packet) {
+		Client.Server.Acknowledge(Packet)
 	})
 
-	Server.On("Data", func(Client *NEX.Client, Packet *NEX.Packet) {
-		//stream := NEX.NewInputStream(Packet.RMCRequest.Parameters)
-		var response NEX.RMCResponse
-		//responseStream := NEX.NewOutputStream()
+	go AuthServer.Listen(":60000")
 
-		ResponsePacket := NEX.NewPacket(Client)
-
-		ResponsePacket.SetVersion(0)
-		ResponsePacket.SetSource(Packet.Destination)
-		ResponsePacket.SetDestination(Packet.Source)
-		ResponsePacket.SetType(NEX.Types["Data"])
-
-		pid := 1234567890          // account PID/NEX username (dummy)
-		password := "nex_password" // account NEX password (dummy)
-
-		key := []byte(password)
-
-		for i := 0; i < 65000+pid%1024; i++ {
-			key = NEX.MD5Hash(key)
-		}
-
-		//Kerberos := NEX.NewKerberos(string(key))
-		proto_id := Packet.RMCRequest.ProtocolID & ^uint8(0x80)
-		proto, ok := NEXProtocols.Protocols[proto_id]
-
-		if ok {
-			method, ok := proto.Methods[Packet.RMCRequest.MethodID]
-			if ok {
-				response = method(Packet.RMCRequest)
-			} else {
-				// response := failure
-				panic(fmt.Sprintf("Unimplemented method %08x in protocol %02x!", Packet.RMCRequest.MethodID, proto_id))
-			}
-		} else {
-			// response := failure
-			panic(fmt.Sprintf("Unimplemented protocol id %02x!", proto_id))
-		}
-
-		ResponsePacket.SetPayload(response.Bytes())
-		Server.Send(Client, &ResponsePacket)
-	})
-
-	Server.Listen(":60000")
+	SecureServer := createNexServer(settings)
+	SecureServer.On("Connect", secureServerHandleConnect)
+	SecureServer.Listen(":60001")
 }
--- protocols/protocols.go
+++ protocols/protocols.go
@@ -3,6 +3,7 @@ package protocols
 import (
 	NEX "github.com/Stary2001/nex-go"
 	"encoding/json"
+	"encoding/binary"
 )
 
 type NEXMethod func(NEX.RMCRequest) (NEX.RMCResponse)
@@ -58,17 +59,23 @@ func Authentication_RequestTicket(user_pid NEX.PID, server_pid NEX.PID) (rmcResu
 		key = NEX.MD5Hash(key)
 	}
 	Kerberos := NEX.NewKerberos(string(key))
-	str := "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" // dummy data
-
-	ticket = Kerberos.Encrypt([]byte(str))
+	str := "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04" // dummy data
+	data := make([]byte, 4)
+	binary.LittleEndian.PutUint32(data, uint32(user_pid))
+	ticket = Kerberos.Encrypt(append([]byte(str), data...))
 	return
 }
 
 func RegisterProtocols() {
 	Protocols = make(map[uint8]NEXProtocol)
 	Protocols[0xa] = NEXProtocol { 0xa, make(map[uint32]NEXMethod) }
+	Protocols[0xb] = NEXProtocol { 0xb, make(map[uint32]NEXMethod) }
+
 	Protocols[0xa].Methods[1] = Authentication_Login_Wrapper
 	Protocols[0xa].Methods[3] = Authentication_RequestTicket_Wrapper
+
+	//Protocols[0xb].Methods[1] = Secure_Connection_Register_Wrapper
+	Protocols[0xb].Methods[4] = Secure_Connection_RegisterEx_Wrapper
 }
 
 var Protocols map[uint8]NEXProtocol
